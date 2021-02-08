9f1db9acbe8f95079805ffd5e5ea8b6fb34d1366
ChilasDipo <Chil0041@edu.easj.dk>
2021-02-08 12:27:34 +0100

    trying to fix my github

--- Minecraft/src/Dirt.java
+++
@@ -1,5 +0,0 @@
-public class Dirt {
-    public Dirt() {
-        Game.nameobejct("Dirt");
-    }
-}
--- Minecraft/src/Sheep.java
+++
@@ -1,8 +0,0 @@
-public class Sheep {
-    Head head;
-    Body body;
-    Legs leg1 ,leg2 ,leg3 ,leg4;
-    public Sheep(){
-        Game.nameobejct("Sheep");
-    }
-}
